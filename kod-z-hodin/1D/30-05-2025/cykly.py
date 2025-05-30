35b57beb34daeabde562af852ee51caca3a3334c
Denio21 <43010190+Denio21@users.noreply.github.com>
2025-05-30 11:20:35 +0200

    Update cykly.py

--- kod-z-hodin/1D/30-05-2025/cykly.py
+++ kod-z-hodin/1D/30-05-2025/cykly.py
@@ -11,3 +11,23 @@ for i in range(10):
     if i == 3:
         break
     print("😶"*i)
+    
+    
+while True:
+    print("Nekonečný cyklus AAAAAAAAAA")
+    end = input("Napiš ano, aby byl už konec: ")
+    if end == "ano":
+        break
+
+while len(students)>0:
+    for item in students:
+        print(item)
+
+for i in range(20):
+    if(i == 10):
+        print("Byla detekována 10!")
+        continue
+    print("😯"*i)
+    
+    
+print("Cyklus byl roztrhnut")
